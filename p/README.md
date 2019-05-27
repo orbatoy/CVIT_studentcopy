686c0e3a34f7d24af7411d19d240cb55ea6201d7
Aub Thompson <aubthompson@gmail.com>
2019-05-27 11:49:13 +1000

    update read me
    update readme with basic content

--- p/README.md
+++ p/README.md
@@ -1,1 +1,32 @@
-# CVIT_studentcopy
+<h1 align="center">
+<br>
+  <img src="https://cvit.info/p/images/logo_contact.png" alt="GVIT.info" width="125">
+  <br>
+    <br>
+  CVIT.info Student Copy 
+  <br>
+</h1>
+
+# CVIT Student Copy
+### Created by : _Aub Thompson_ for the purpose of introducing web devlopment to students.  
+This project is for use by TAFE students to see CVIT.info at various stages of development and experiment with their own live version
+
+## Table of Contents
+
+1. **[How to use](#how)**
+2. **[local setup with XAMPP](#local)**
+3. **[Theme](#theme)**
+4. **[PHP](#PHP)**
+5. **[Database](#dbase)**
+6. **[Performance](#performance-1)**
+7. **[Accessibility](#accessibility)**
+8. **[SEO](#seo)**
+
+
+---
+
+## How to use?
+
+
+
+## local setup with XAMPP
---
+++ p/lanparties.php
@@ -0,0 +1,168 @@
+<?php 
+include 'includes/conn.php';
+include 'cvittop.php'; ?>
+
+
+<section class="callout">
+  <div class="row">
+    <div class="five columns">
+      <h2 class="title">LAN Parties</h2>
+    </div>
+    <div class="seven columns">
+<ul class="breadcrumbs">
+  <li><span><a href="index.html">Home</a></span></li>
+  <li class="current"><span><a href="lanparties.html">Lan Parties</a></span></li>
+</ul>
+    </div>
+  </div>
+</section>
+
+
+  
+
+
+
+
+
+<section class="main">
+
+
+<div class="row">
+  <div class="six columns">
+<div class="flex-video">
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/JSRD43vFT58?rel=0" allowfullscreen width="500" height="160" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+</div>
+<hr>
+<h4>Games Played</h4>
+  <p> PC games and competitions are decided through Facebook polls and post prior to the event and these are often included; COD, Halo, Counter Strike, League of Legends, Unturned, Rocket League and Super Smash Bros</p> 
+
+
+</div>
+  <div class="six columns">
+<h3>Grafton TAFE LAN Party</h3>
+<hr>
+      <p>Every few months LAN Gaming events are held at Grafton TAFE. Supported by Grafton TAFE, TVET, IT Students and supervised by IT Teacher in Charge Mr Aubrey Thompson.</p> 
+
+<p> Cost is $15 which covers Pizza, drinks, facilities and occasionally some prizes. Some events include consoles (Xbox,  PlayStation and Wii u, etc.) those interested organise it via facebook posts. We will keep you updated on new ideas, times and games and if something has changed we will tell you as soon as possible. </p>
+
+<p> Positions are limited so you need to be 100% sure if you are going and if you can't tell us as soon as possible. Bringing your own PC's is preferred however basic TAFE computers are occasionally supplied. Please tell us before so we can make room. No alcohol will be permitted on the night. Anyone caught drinking or behaving offensively will be removed.</p>
+
+
+<p> Please <a href="contact.php">contact us</a> if you can assist or sponsors an event.</p>
+<div class="right"><a class="button" href="https://www.facebook.com/groups/GraftonTAFELANPARTY/" target="_blank"><i class="icon-facebook"></i> Join Facebook Group </a>
+<br><br>
+<a class="button" href="http://surveymonkey.com/r/YN69CWS" target="_blank"> Share your feedback </a></div>
+</div>
+</div>
+
+
+
+
+
+<div class="spacer2"></div>
+
+
+  <div class="row mb20">
+    <div class="twelve mobile-four columns">
+<h1 class="large">Gallery</h1>
+</div>
+  </div>
+
+
+  <div class="row">
+<div class="six mobile-four columns mb20">
+<figure><img src="images/langamers4.jpg" alt="" data-bottom-center="transform:scale(2)" data-center-center="transform:scale(1)">
+<figcaption><span>&#169; Copyright</span></figcaption></figure>
+</div>
+
+
+<div class="three mobile-two columns mb20">
+<figure><img src="images/langamers13.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+<div class="three mobile-two columns mb20">
+<figure>
+<img src="images/langamers14.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+
+</div>
+
+
+
+  <div class="row">
+    <div class="six mobile-four columns mb20">
+<figure >
+<img src="images/langamers17.jpg" alt="" data-bottom-center="transform:scale(2)" data-center-center="transform:scale(1)">
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+
+
+    <div class="six mobile-four columns mb20">
+<figure>
+<img src="images/langamers2.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+</div>
+
+ <div class="row mb20">
+    <div class="three mobile-two columns mb20">
+<figure>
+<img src="images/langamers15.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+
+    <div class="six mobile-four columns mb20">
+<figure>
+<img src="images/langamers6.jpg" alt="">
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+
+    <div class="three mobile-two columns mb20">
+<figure>
+<img src="images/langamers9.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+    
+</div>
+  <div class="row mb20">
+    <div class="six mobile-four columns mb20">
+<figure>
+<img src="images/langamers16.jpg" alt="" />
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+
+
+    <div class="six mobile-four columns mb20">
+<figure>
+<img src="images/langamers5.jpg" alt="">
+<figcaption><span>&#169; </span></figcaption></figure>
+</figure>
+</div>
+    
+</div>
+
+
+
+
+
+<div class="spacer10"></div>
+
+
+
+</section>
+
+
+
+
+
+
+<?php include 'cvitfooter.php' ?>

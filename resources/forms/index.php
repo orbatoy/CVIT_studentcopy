757478a338aff89b54c88240fded3ed3442ac457
Aub Thompson <aubthompson@gmail.com>
2019-05-27 10:54:12 +1000

    Folders & sample content
    Include additional root director folders and sample data

---
+++ resources/forms/index.php
@@ -0,0 +1,35 @@
+<?php    
+
+header('Location: /p/index.php');    
+
+
+
+/*
+
+
+
+
+
+
+
+moodle 3.1.2+ - moodle-latest-31.zip 
+
+moodle theme purchased under account orbatoy aubrey.thompson@tafensw.edu.au
+
+http://preview.themeforest.net/item/lambda-responsive-moodle-theme/full_screen_preview/9442816?_ga=1.54187944.1671994356.1476763116
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+?> 
